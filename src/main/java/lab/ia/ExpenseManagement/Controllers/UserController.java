d330b6ed12730825bfdf37a27fcb0207b566029f
tieu hoa <tieutrunghoa1198@gmail.com>
2023-06-12 17:16:13 +0700

    add cors for category and user

--- src/main/java/lab/ia/ExpenseManagement/Controllers/UserController.java
+++ src/main/java/lab/ia/ExpenseManagement/Controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
